d6a584c1e2032b22ac77c1c19dabbea20545ff25
SunSet0418 <coffee@soylatte.kr>
2018-04-16 13:22:17 +0900

    Add ResponseEntity for send HTTP code

--- src/main/java/Controller/Auth.java
+++ src/main/java/Controller/Auth.java
@@ -3,7 +3,10 @@ package Controller;
 import DAO.Mapper;
 import Model.UserModel;
 import com.fasterxml.jackson.core.JsonParser;
+import org.omg.PortableInterceptor.SUCCESSFUL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
@@ -20,62 +23,74 @@ public class Auth {
     private Mapper mapper;
 
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestParam("id") String id, @RequestParam("password") String password){
+    public ResponseEntity<Map<String, Object>> login(@RequestParam("id") String id, @RequestParam("password") String password){
 
         Map<String, Object> return_data = new HashMap<String, Object>();
+
+        ResponseEntity res_data;
 
         UserModel result_data = mapper.getUser(id, password);
 
         if(result_data == null){
             return_data.put("success", false);
             return_data.put("message", "User Not Founded");
+            res_data = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(return_data);
         }
         else{
             return_data.put("username", result_data.getUsername());
             return_data.put("id", result_data.getId());
             return_data.put("password", result_data.getPassword());
+            res_data = ResponseEntity.ok(return_data);
         }
 
-        return return_data;
+        return res_data;
+
     }
 
     @PostMapping("/register")
-    public String register(UserModel r_user){
+    public ResponseEntity<String> register(UserModel r_user){
 
-        String return_data;
+        String return_msg;
+
+        ResponseEntity code;
 
         UserModel checkdata = mapper.checkUser(r_user.getId());
 
         if(checkdata == null){
             mapper.insertUser(r_user.getUsername(), r_user.getId(), r_user.getPassword());
-            return_data = "success";
+            return_msg = "success";
+            code = ResponseEntity.ok(return_msg);
         }
         else{
-            return_data = "fail";
+            return_msg = "fail";
+            code = ResponseEntity.status(409).body(return_msg);
         }
 
-        return return_data;
+        return code;
 
     }
 
-    @DeleteMapping("/delete")
-    public String delete(@RequestParam String body){
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") String id){
 
         String return_msg;
 
-        String[] r_parse = body.split("=");
-        System.out.println("DELETE ID : "+r_parse[1]);
-        UserModel check = mapper.checkUser(r_parse[1]);
+        ResponseEntity code;
+
+        UserModel check = mapper.checkUser(id);
 
         if(check == null){
             return_msg = "User Not Founded In Database";
+            code = ResponseEntity.status(400).body(return_msg);
         }
         else {
-            mapper.deleteUser(r_parse[1]);
+            mapper.deleteUser(id);
             return_msg = "Success";
+            code = ResponseEntity.ok(return_msg);
         }
 
-        return return_msg;
+        return code;
+
     }
 
 
